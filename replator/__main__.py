d57ff5f9922afb0a9ebfdeca9135bef8472ca2d9
AlexV <asmodehn@gmail.com>
2018-05-24 17:01:03 +0200

    trying out prompt_toolkit

--- replator/__main__.py
+++ replator/__main__.py
@@ -3,21 +3,56 @@ import sys
 import replator.display
 
 import click
+from click_repl import register_repl
 
 """
 A Terminal interface to your OS, specialized for stack-based languages.
 """
 
+#
+# @click.command()
+# @click.argument('file')
+# def cli(file):
+#     try:
+#         name = sys.argv[1]
+#         assert open(name, "a")
+#     except:
+#         sys.stderr.write(__doc__)
+#         return
+#     replator.display.Display(name).main()
 
-def main():
-    try:
-        name = sys.argv[1]
-        assert open(name, "a")
-    except:
-        sys.stderr.write(__doc__)
-        return
-    replator.display.Display(name).main()
+
+# @cli.command()
+# def calc():
+#     """
+#     running the calc interpreter
+#     """
+#
+#
+#
+# @cli.command()
+# def bfck():
+#     """
+#     running the bfck interpreter
+#     """
 
 
-if __name__=="__main__":
-    main()
+
+# def main():
+#     try:
+#         name = sys.argv[1]
+#         assert open(name, "a")
+#     except:
+#         sys.stderr.write(__doc__)
+#         return
+#     replator.display.Display(name).main()
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def hello():
+    click.echo("Hello world!")
+
+register_repl(cli)
